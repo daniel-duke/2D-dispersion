8f3d57c4545feea49e948c9521b32ec7d1c58c87
alex ogren <aco8ogren@gmail.com>
2020-09-10 16:16:50 -0700

    Update dispersion_script.m to align with recent changes

--- dispersion_script.m
+++ dispersion_script.m
@@ -5,10 +5,10 @@ const.N_pix = 4;
 const.N_k = 30;
 const.N_eig = 8;
 const.isUseGPU = false;
-const.isUseImprovement = false;
+const.isUseImprovement = true;
 const.isUseParallel = true;
 
-const.wavevectors = create_wavevector_array(const.N_k,const.a);
+const.wavevectors = create_IBZ_boundary_wavevectors(const.N_k,const.a);
 % const.wavevectors = [0, 1.309, pi/2, pi/const.a;
 %                      0, 0,     0,    0        ];
 
@@ -58,13 +58,11 @@ plot_design(const.design);
 [wv,fr,ev] = dispersion(const,const.wavevectors);
 fr(:,end + 1) = fr(:,1);
 ev(:,end + 1,:) = ev(:,1,:);
-[ev,theta] = align_eigenvectors(ev,const);
 wn = linspace(0,3,size(const.wavevectors,2) + 1);
 wn = repmat(wn,const.N_eig,1);
 
 %% Plot the discretized Irreducible Brillouin Zone
 plot_wavevectors(wv);
-
 
 %% Plot the dispersion relation
 figure2();
@@ -77,38 +75,3 @@ plot_dispersion(wn,fr);
 % plot_mode_ui(wv,fr,ev,const);
 % plot_mode(wv,fr,ev,eig_idx,k_idx,'both',const)
 
-
-isSaveOutput = true;
-eig_idx = 5;
-if isSaveOutput
-    script_start_time = replace(char(datetime),':','-');
-    output_folder = ['OUTPUT/output ' script_start_time];
-    mkdir(output_folder);
-    copyfile([mfilename('fullpath') '.m'],[output_folder '/' mfilename '.m']);
-else
-    output_folder = [];
-    disp('WARNING: isSaveOutput is set to false. Output will not be saved.')
-end
-
-fig = figure2();
-set(fig,'units','normalized');
-set(fig,'outerposition',[0 0 1 1]);
-VideoWriterObj = VideoWriter([output_folder '/mode_tracking_by_eig_idx ' script_start_time],'MPEG-4');
-VideoWriterObj.FrameRate = 2;
-open(VideoWriterObj);
-for k_idx = 1:(size(wn,2) - 1)
-    subplot(1,2,1)
-    plot_dispersion(wn,fr);
-    scatter(wn(1,k_idx),fr(eig_idx,k_idx),'ro');
-    
-    subplot(1,2,2)
-    plot_mode(wv,fr,ev,eig_idx,k_idx,'stillui',0,const);
-    
-    writeVideo(VideoWriterObj,getframe(gcf))
-end
-close(VideoWriterObj);
-
-figure2();
-plot(theta(eig_idx,:));
-xlabel('k_idx')
-ylabel('\theta')
